b15987e112f05d15f773b96a1d60ef544c47ab93
Sofia Carena <sofia.carena@globant.com>
2021-11-16 12:43:03 -0300

    solve exercise 2 constraints

---
+++ constraints_exercise_2/constraints_exercise_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  constraints_exercise_2
+//
+//  Created by Sofia Carena on 03/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
