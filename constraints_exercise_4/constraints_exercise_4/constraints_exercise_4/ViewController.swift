f42ccf52a51a7da4c385a90ff88a9b20b3917bf6
Sofia Carena <sofia.carena@globant.com>
2021-11-19 05:23:08 -0300

    constraints exercise 4 layout without buttons border

---
+++ constraints_exercise_4/constraints_exercise_4/constraints_exercise_4/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  constraints_exercise_4
+//
+//  Created by Sofia Carena on 18/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension UIButton {
+
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.borderColor = color.cgColor
+            } else {
+                layer.borderColor = nil
+            }
+        }
+    }
+}
